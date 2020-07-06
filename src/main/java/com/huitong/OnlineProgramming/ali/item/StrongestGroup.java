87b9babec74e94cafde8bfc98a5c568663ec7ed6
GodWine <xieyutong_king@163.com>
2020-07-06 22:50:05 +0800

    阿里在线编程 -> 最强的团队 -> remove unused imports

--- src/main/java/com/huitong/OnlineProgramming/ali/item/StrongestGroup.java
+++ src/main/java/com/huitong/OnlineProgramming/ali/item/StrongestGroup.java
@@ -1,6 +1,5 @@
 package com.huitong.OnlineProgramming.ali.item;
 
-import jdk.nashorn.internal.objects.annotations.SpecializedFunction;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
